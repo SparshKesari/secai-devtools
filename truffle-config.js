16859150c1c98d107be22d2129fe6083550ee243
Sparsh Kesari <39815871+SparshKesari@users.noreply.github.com>
2020-12-21 17:40:57 +0530

    dev tools
    dev tools

---
+++ truffle-config.js
@@ -0,0 +1,40 @@
+// require('babel-register');
+// require('babel-polyfill');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const path = require("path");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+
+  // contracts_build_directory:path.join(__dirname,"dev/src/contracts"),
+ 
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+    }
+  }
+}
+
